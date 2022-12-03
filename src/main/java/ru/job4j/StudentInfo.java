18f90d0be4331111899ed9e258869eeabc43cf7e
NeruAlex <nerualex@mail.ru>
2022-12-03 22:22:54 +0900

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,6 +1,6 @@
 package ru.job4j;
 public class StudentInfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Alexandr Neshcheretov");
         System.out.println("26.08.1992");
     }
