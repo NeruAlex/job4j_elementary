3f6897a99d634581463a3d436df4e8521ae3d904
NeruAlex <nerualex@mail.ru>
2022-12-03 21:31:21 +0900

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Alexandr Neshcheretov");
+        System.out.println("26.08.1992");
+    }
+}
